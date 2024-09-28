2acca095d0a31151f404bc14dfd6b5396870f9fe
Elvis Kuria <levonjoroge>
2024-09-28 14:50:26 +0300

    created index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+let grade = 75
+
+if (grade >=0 && grade <=40){
+    console.log("E")
+}
+else if(grade >=41 && grade <=50){
+    console.log("D")
+}
+else if(grade >=51 && grade <=60){
+    console.log("C")
+}
+else if(grade >=61 && grade <=70){
+    console.log("B")
+}
+else if(grade >= 71 && grade <=100){
+    console.log("A")
+}
+else {
+    console.log("Fail")
+}
